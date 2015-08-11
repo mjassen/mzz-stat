6db792b467c7818f0027a769b9b035c776150c02
mjassen <mjassen@users.noreply.github.com>
2015-08-10 20:53:33 -0400

    initial commit
    initial commit

---
+++ mzz-stat.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Plugin Name: Mzz-stat
+* Plugin URI: https://github.com/mjassen/mzz-stat
+* Description: A plugin that records statistics for a WordPress site.
+* Version: 0.0.1
+* Author: mjassen
+* Author URI: http://wieldlinux.com/
+* License: MIT
+*/
+
+
+/*
+
+*/
+
+add_action( 'wp_footer', 'mzz_include_mzzstat', 99 );
+function mzz_include_mzzstat() {
+
+}
+?>
