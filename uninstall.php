049ec98e891816fc8f3e043eeecde589b4238c81
mjassen <mjassen@users.noreply.github.com>
2016-03-21 06:45:06 -0400

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -6,7 +6,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 function mzzstat_delete_plugin() {
 	global $wpdb;
 
-	$table_name = $wpdb->prefix . "mzzstat";
+	$table_name = $wpdb->prefix . "mzzstat_v2";
 
 	$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 }
